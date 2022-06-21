9705720e866035e42885373bbeff2b9d380d544a
Diego.Pigarri <diego.pigarri@gmail.com>
2022-06-20 21:11:15 -0300

    UPDATE para Livro implementado

--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 import { environment } from 'src/environments/environment';
 import { Livro } from './livro.model';
 
@@ -19,13 +20,23 @@ export class LivroService {
     return this.http.get<Livro[]>(url)
   }
 
+  findById(id: String): Observable<Livro> {
+    const url = `${this.baseUrl}/livros/${id}`
+    return this.http.get<Livro>(url)
+  }
+
+  update(livro: Livro): Observable<Livro> {
+    const url = `${this.baseUrl}/livros/${livro.id}`
+    return this.http.put<Livro>(url, livro)
+  }
+
   create(livro: Livro, id_cat: String): Observable<Livro> {
     const url = `${this.baseUrl}/livros?categoria=${id_cat}`
     return this.http.post<Livro>(url, livro)
   }
 
   mensagem(str: String): void {
-    this._snack.open(`${str}`, 'OK',{
+    this._snack.open(`${str}`, 'OK', {
       horizontalPosition: 'end',
       verticalPosition: 'top',
       duration: 3000
